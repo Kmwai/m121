9ac8cbd31beede035ddd198d895e6d1123d25763
Mwai Karimi <mwaikarimi@Mwais-MacBook-Pro.local>
2017-11-07 16:57:02 +0000

    lab 2 $unwind sol

---
+++ chap_3/lab2.js
@@ -0,0 +1,40 @@
+/*Let's use our increasing knowledge of the Aggregation Framework to explore our movies collection in more detail. We'd like to calculate how many movies every cast member has been in and get an average imdb.rating for each cast member.
+
+What is the name, number of movies, and average rating (truncated to one decimal) for the cast member that has been in the most number of movies with English as an available language?
+
+Provide the input in the following order and format
+{ "_id": "First Last", "numFilms": 1, "average": 1.1 } */
+
+var y = db.movies.aggregate([
+    {
+        $match: {
+            'languages': 'English',
+            'imdb.rating': { $gte: 0 }
+        }
+    },
+    {
+        $unwind: '$cast'
+    },
+    {
+        $group: {
+            _id: '$cast',
+            numFilms: { $sum: 1 },
+            average: { $avg: '$imdb.rating' }
+        }
+    },
+    {
+        $sort: {
+            "numFilms": -1
+        }
+    },
+    {
+        $limit: 1
+    },
+    {
+        $project: {
+            average: { $divide: [{ $trunc: { $multiply: ['$average', 10] } }, 10] },
+            numFilms: 1
+        }
+    }
+
+])
