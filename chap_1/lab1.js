5e0733734b3a0f05946f2f3aa9a56d2322faa949
Mwai <mwai@Misfit.home>
2018-03-18 14:06:08 +0000

    fix

--- chap_1/lab1.js
+++ chap_1/lab1.js
@@ -1,17 +1,10 @@
-var x = [
+var pipeline = [
     {
         $match: {
             'imdb.rating': { $gte: 7 },
             'genres': { $nin: ['Crime', 'Horror'] },
             'rated': { $in: ['PG', 'G'] },
             $and: [{ 'languages': 'English' }, { 'languages': 'Japanese' }]
-        }
-    },
-    {
-        $project: {
-            title: 1,
-            rated: '$imdb.rating',
-            _id: 0
-        }
+        }  
     }
 ]
